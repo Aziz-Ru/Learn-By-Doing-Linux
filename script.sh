d814ff38cda24dc5fe7b3d92ac2fcaa98b5553ef
aziz <siliconoxide4@gmail.com>
2024-08-12 10:53:49 +0600

    Added script

---
+++ script.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+sudo apt autoclean
+nvm install 22
+sudo apt install git
+sudo apt install gcc
+sudo apt install gnome-shell-extension-manager
+sudo apt install libreoffice
+sudo add-apt-repository ppa:maarten-fonville/android-studio 
+sudo apt update
+sudo apt install android-studio
+# df -h
